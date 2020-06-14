5bc141b1ca3b85ab1e45328d617e1add7e800ecf
Paras Rajput <paras.code.api@gmail.com>
2020-06-14 11:40:25 +0530

    Add project files.

---
+++ StorageAccountSolutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StorageAccountSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
